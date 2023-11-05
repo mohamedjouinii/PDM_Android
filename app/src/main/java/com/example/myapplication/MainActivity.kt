dbf52891be7ffe097e2293a7c8edd094e5eab6f2
saifmessaoudi <saifmessaoudi99@gmail.com>
2023-11-05 15:08:26 +0100

    init android

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,16 +2,31 @@ package com.example.myapplication
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityLoginBinding
-
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : ComponentActivity() {
 
-     lateinit var binding: ActivityLoginBinding
+
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+       installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.textForgetPassword.setOnClickListener {
+           Snackbar.make(it, "Forget Password soon", Snackbar.LENGTH_LONG).show()
+        }
+
+        binding.button.setOnClickListener(){
+            Snackbar.make(it, "Login soon", Snackbar.LENGTH_LONG).show()
+        }
     }
+
+
+
 }
